56581565c58bf88808d268449a63bd3dbf4effdd
mozhaonan <2885354314@qq.com>
2026-05-19 11:44:32 +0800

    chore(strapi config): 配置服务器超时、请求体与上传文件大小限制及上传插件参数
    设置服务器请求超时时间为10分钟，调整body解析中间件的请求体与上传文件大小限制，配置上传插件的图片断点、缓存规则、安全策略及并发上传数量

--- strapi/first-strapi-project/config/plugins.ts
+++ strapi/first-strapi-project/config/plugins.ts
@@ -19,6 +19,32 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
       },
     },
   },
+  upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          maxage: 300000
+        },
+      },
+      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
+      },
+      sharp: {
+        cache: true,
+        concurrency: 4,
+      },
+      security: {
+        allowedTypes: ['image/*', 'application/*'],
+        deniedTypes: ['application/x-sh', 'application/x-dosexec']
+      },
+      concurrentUploadSize: 5,
+    },
+  },
 });
 
 export default config;
--- strapi/first-strapi-project/config/server.ts
+++ strapi/first-strapi-project/config/server.ts
@@ -6,6 +6,11 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server =>
   app: {
     keys: env.array('APP_KEYS'),
   },
+  http: {
+    serverOptions: {
+      requestTimeout: 10 * 60 * 1000, // 10 minutes timeout
+    }
+  }
 });
 
 export default config;

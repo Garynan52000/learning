deeca1d085b15d031a2b406412e91c3189441963
mozhaonan <2885354314@qq.com>
2026-06-12 18:06:03 +0800

    feat(admin): 配置后台语言支持并完善启动逻辑
    新增StrapiApp类型导入，配置后台支持简体中文与中文语言，注释其他备用语言项以便后续启用，同时完善bootstrap函数并打印app实例。

--- strapi/first-strapi-project/src/admin/app.ts
+++ strapi/first-strapi-project/src/admin/app.ts
@@ -1,3 +1,37 @@
+import type { StrapiApp } from '@strapi/strapi/admin';
+
 export default {
-  bootstrap() {},
+  config: { // https://docs.strapi.io/cms/admin-panel-customization#available-customizations
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pl',
+      // 'pt-BR',
+      // 'pt',
+      // 'ru',
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      'zh-Hans',
+      'zh',
+    ],
+  },
+  bootstrap(app: StrapiApp) {
+    console.log(app);
+  },
 };
